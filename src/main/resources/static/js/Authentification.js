2ea00971de37a03b881c244f624d5e5665131a7c
unknown <medjaher.ismet@gmail.com>
2017-05-13 02:42:13 +0200

    myApp

---
+++ src/main/resources/static/js/Authentification.js
@@ -0,0 +1,33 @@
+/**
+ * Ismet
+ * 
+ */
+var app=angular.module("myApp14",[])
+
+
+
+app.controller("authentification", function($scope,$http) {
+	console.log("Let's start the show")
+	
+	$scope.authentification=function(){
+		$scope.medecin={}
+		console.log("je suis rentrer")
+		
+		
+		$http.get("Authentification/"+$scope.email+"/"+$scope.password)
+		.success(function(data) {
+			$scope.medecin=data
+			if($scope.medecin.email == $scope.email){
+				
+				 alert("Salut "+$scope.medecin.statut+" "+$scope.medecin.nom)
+				document.location.href="AcceuilMedecin.html"
+			}else{
+				 alert("erreur id ou mot de passe incorrect");
+				 document.location.href="ChoixLogin.html"
+			}
+		
+		});
+		
+	}
+	
+	});
